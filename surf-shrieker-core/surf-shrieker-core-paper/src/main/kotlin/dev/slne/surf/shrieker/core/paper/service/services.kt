4a07fe0346e7a23dc4d602c4f241d97b09770a86
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-06-02 17:22:49 +0200

    feat: add ban check in /report

--- surf-shrieker-core/surf-shrieker-core-paper/src/main/kotlin/dev/slne/surf/shrieker/core/paper/service/services.kt
+++ surf-shrieker-core/surf-shrieker-core-paper/src/main/kotlin/dev/slne/surf/shrieker/core/paper/service/services.kt
@@ -1,5 +1,6 @@
 package dev.slne.surf.shrieker.core.paper.service
 
+import dev.slne.surf.shrieker.core.common.service.ReportBanService
 import dev.slne.surf.shrieker.core.common.service.ReportService
 import dev.slne.surf.shrieker.core.common.service.VoiceLogService
 
@@ -12,6 +13,11 @@ object ReportServices {
         TODO("Implement service discovery and retrieval")
     }
 
+    private val reportBanService: ReportBanService by lazy {
+        TODO("Implement service discovery and retrieval")
+    }
+
     fun voiceLog() = voiceLogService
     fun report() = reportService
+    fun reportBan() = reportBanService
 }
--- surf-shrieker-paper/src/main/kotlin/dev/slne/surf/shrieker/paper/command/ReportCommand.kt
+++ surf-shrieker-paper/src/main/kotlin/dev/slne/surf/shrieker/paper/command/ReportCommand.kt
@@ -2,11 +2,14 @@ package dev.slne.surf.shrieker.paper.command
 
 import dev.jorel.commandapi.kotlindsl.commandTree
 import dev.slne.surf.api.core.command.args.awaiting
+import dev.slne.surf.api.core.messages.adventure.sendText
 import dev.slne.surf.api.paper.command.executors.playerExecutorSuspend
 import dev.slne.surf.core.api.common.player.SurfPlayer
 import dev.slne.surf.core.api.paper.command.argument.surfOfflinePlayerArgument
+import dev.slne.surf.shrieker.core.paper.service.ReportServices
 import dev.slne.surf.shrieker.paper.permission.PermissionList
 import dev.slne.surf.shrieker.paper.ui.dialog.reportTypeDialog
+import dev.slne.surf.shrieker.paper.util.appendShriekerPrefix
 
 fun reportCommand() = commandTree("report") {
     withPermission(PermissionList.REPORT_COMMAND)
@@ -14,6 +17,14 @@ fun reportCommand() = commandTree("report") {
     surfOfflinePlayerArgument("target") {
         playerExecutorSuspend { player, arguments ->
             val target = arguments.awaiting<SurfPlayer>("target")
+
+            if (ReportServices.reportBan().isBanned(player.uniqueId)) {
+                player.sendText {
+                    appendShriekerPrefix()
+                    error("Du kannst keinen Report erstellen, da du von der Reportfunktion ausgeschlossen wurdest.")
+                }
+                return@playerExecutorSuspend
+            }
 
             player.showDialog(
                 reportTypeDialog(
